abc7df5046baf1e7d56da1bb267624f88043562c
Thang612 <dangtrungthang6122003@gmail.com>
2024-04-21 03:03:45 +0700

    add comment for func search and update

--- TestDivuiWebsite/Search.cs
+++ TestDivuiWebsite/Search.cs
@@ -14,129 +14,179 @@ namespace TestDivuiWebsite
     [TestClass]
     public class Search
     {
+        // Khai báo một biến driver để sử dụng cho việc điều khiển của trình duyệt web
         private IWebDriver driverThang_63;
+        // Khai báo url của trang web chứa chức năng cần test
         private string urlThang_63 = "https://divui.com/blog";
+        //Khai báo một biến các cấu hình của Chrome 
         private ChromeOptions optionsThang_63 = new ChromeOptions();
 
-
+        // Phương thức thiết lập trước mỗi bài kiểm tra (test) để chuẩn bị môi trường và dữ liệu cần thiết
+        //Phương thức này dùng để truy cập vào các bước đầu tiên chung của các test case 
         [TestInitialize]
         public void SetUp()
         {
+            // Đường dẫn đến file chromedriver.exe
             string chromeDriverPath = "chromedriver.exe";
+
+            // Thêm tùy chọn để khởi động trình duyệt Chrome với cửa sổ lớn nhất
             optionsThang_63.AddArgument("--start-maximized");
+
+            // Tạo một phiên làm việc với trình duyệt Chrome sử dụng chromedriver và các tùy chọn đã cấu hình
             driverThang_63 = new ChromeDriver(chromeDriverPath, optionsThang_63);
+
+            // Mở trình duyệt và điều hướng đến URL cụ thể
             driverThang_63.Navigate().GoToUrl(urlThang_63);
         }
 
+        //Phương thức này dùng để kiểm tra tìm kiểm với dữ liệu rỗng
         [TestMethod]
         public void checkEmptyThang_63()
         {
+            //Tìm element biểu tượng tìm kím và tiến hành nhấn để hiển thị ô input của tìm kiếm
             IWebElement btnSearchThang_63 = driverThang_63.FindElement(By.CssSelector("i.td-icon-search:first-child"));
             btnSearchThang_63.Click();
             Thread.Sleep(1000);
 
+            //Lấy element của ô input tìm kiếm và tíến hành gửi dữ liệu 
             IWebElement searchInputThang_63 = driverThang_63.FindElement(By.Id("td-header-search"));
             searchInputThang_63.Clear();
+            //Gửi dữ liệu rỗng để tiến hành kiếm tra
             searchInputThang_63.SendKeys("");
             Thread.Sleep(1000);
 
+            //Lấy element của button tìm kiếm để tiến hành tìm kiếm với gía trị 
             IWebElement btnInputThang_63 = driverThang_63.FindElement(By.Id("td-header-search-top"));
             btnInputThang_63.Click();
             Thread.Sleep(5000);
 
+            //Láy danh sách các element blog bằng Class 
             IList<IWebElement> blogsThang_63 = driverThang_63.FindElements(By.CssSelector(".td_module_16"));
+            //Kiểm tra danh sách trên có nhiều hơn 0 hay không nêú hơn thì blog đã được tìm kiếm và ngược lại 
             Assert.IsTrue(blogsThang_63.Count > 0, "No blogs found.");
         }
 
         [TestMethod]
         public void checkTextFailThang_63()
         {
+            //Tìm element biểu tượng tìm kím và tiến hành nhấn để hiển thị ô input của tìm kiếm
             IWebElement btnSearchThang_63 = driverThang_63.FindElement(By.CssSelector("i.td-icon-search:first-child"));
             btnSearchThang_63.Click();
             Thread.Sleep(1000);
 
+            //Lấy element của ô input tìm kiếm và tíến hành gửi dữ liệu 
             IWebElement searchInputThang_63 = driverThang_63.FindElement(By.Id("td-header-search"));
             searchInputThang_63.Clear();
+            //Gửi dữ liệu số để tiến hành kiếm tra
             searchInputThang_63.SendKeys("A@!#*123fd");
             Thread.Sleep(1000);
 
+            //Lấy element của button tìm kiếm để tiến hành tìm kiếm với gía trị 
             IWebElement btnInputThang_63 = driverThang_63.FindElement(By.Id("td-header-search-top"));
             btnInputThang_63.Click();
-            Thread.Sleep(10000);
+            Thread.Sleep(5000);
 
+            //Lấy thông báo hiển thị thông báo không tìm thấy blog 
             IWebElement resultThang_63 = driverThang_63.FindElement(By.CssSelector(".no-results>h2"));
+            //Nếu có thông báo thì test case thành công và ngược lại
             Assert.IsNotNull(resultThang_63, "Element message not found. Testcase fail");
+            //Kiểm tra nội dung thông báo
             Assert.AreEqual(resultThang_63.Text, "No results for your search");
-
+            //Lấy danh sách các element blog bằng Class 
+            IList<IWebElement> blogsThang_63 = driverThang_63.FindElements(By.CssSelector(".td_module_16"));
+            //Kiểm tra danh sách trên có bằng 0 hay không nêú hơn thì blog đã được tìm kiếm và ngược lại 
+            Assert.IsTrue(blogsThang_63.Count == 0, "No blogs found.");
         }
 
         [TestMethod]
         public void checkTextSuccessThang_63()
         {
+            //Tìm element biểu tượng tìm kím và tiến hành nhấn để hiển thị ô input của tìm kiếm
             IWebElement btnSearchThang_63 = driverThang_63.FindElement(By.CssSelector("i.td-icon-search:first-child"));
             btnSearchThang_63.Click();
             Thread.Sleep(1000);
 
+            //Lấy element của ô input tìm kiếm và tíến hành gửi dữ liệu 
             IWebElement searchInputThang_63 = driverThang_63.FindElement(By.Id("td-header-search"));
             searchInputThang_63.Clear();
+            //Gửi dữ liệu chuỗi để tiến hành kiếm tra
             searchInputThang_63.SendKeys("Biển");
             Thread.Sleep(1000);
 
+            //Lấy element của button tìm kiếm để tiến hành tìm kiếm với gía trị 
             IWebElement btnInputThang_63 = driverThang_63.FindElement(By.Id("td-header-search-top"));
             btnInputThang_63.Click();
             Thread.Sleep(5000);
 
+            //Lấy danh sách các element blog bằng Class 
             IList<IWebElement> blogsThang_63 = driverThang_63.FindElements(By.CssSelector(".td_module_16"));
+            //Kiểm tra danh sách trên có nhiều hơn 0 hay không nêú hơn thì blog đã được tìm kiếm và ngược lại 
             Assert.IsTrue(blogsThang_63.Count > 0, "No blogs found.");
+
         }
 
         [TestMethod]
         public void checkNumberSuccessThang_63()
         {
+            //Tìm element biểu tượng tìm kím và tiến hành nhấn để hiển thị ô input của tìm kiếm
             IWebElement btnSearchThang_63 = driverThang_63.FindElement(By.CssSelector("i.td-icon-search:first-child"));
             btnSearchThang_63.Click();
             Thread.Sleep(1000);
 
+            //Lấy element của ô input tìm kiếm và tíến hành gửi dữ liệu 
             IWebElement searchInputThang_63 = driverThang_63.FindElement(By.Id("td-header-search"));
             searchInputThang_63.Clear();
+            //Gửi dữ liệu số để tiến hành kiếm tra
             searchInputThang_63.SendKeys("2018");
             Thread.Sleep(1000);
 
+            //Lấy element của button tìm kiếm để tiến hành tìm kiếm với gía trị 
             IWebElement btnInputThang_63 = driverThang_63.FindElement(By.Id("td-header-search-top"));
             btnInputThang_63.Click();
             Thread.Sleep(5000);
 
+            //Lấy danh sách các element blog bằng Class 
             IList<IWebElement> blogsThang_63 = driverThang_63.FindElements(By.CssSelector(".td_module_16"));
+            //Kiểm tra danh sách trên có nhiều hơn 0 hay không nêú hơn thì blog đã được tìm kiếm và ngược lại 
             Assert.IsTrue(blogsThang_63.Count > 0, "No blogs found.");
         }
 
         [TestMethod]
         public void checkSpecialSuccessThang_63()
         {
+            //Tìm element biểu tượng tìm kím và tiến hành nhấn để hiển thị ô input của tìm kiếm
             IWebElement btnSearchThang_63 = driverThang_63.FindElement(By.CssSelector("i.td-icon-search:first-child"));
             btnSearchThang_63.Click();
             Thread.Sleep(1000);
 
+            //Lấy element của ô input tìm kiếm và tíến hành gửi dữ liệu 
             IWebElement searchInputThang_63 = driverThang_63.FindElement(By.Id("td-header-search"));
             searchInputThang_63.Clear();
+            //Gửi dữ liệu số để tiến hành kiếm tra
             searchInputThang_63.SendKeys("@");
             Thread.Sleep(1000);
 
+            //Lấy element của button tìm kiếm để tiến hành tìm kiếm với gía trị 
             IWebElement btnInputThang_63 = driverThang_63.FindElement(By.Id("td-header-search-top"));
             btnInputThang_63.Click();
             Thread.Sleep(5000);
 
+            //Lấy danh sách các element blog bằng Class 
             IList<IWebElement> blogsThang_63 = driverThang_63.FindElements(By.CssSelector(".td_module_16"));
+            //Kiểm tra danh sách trên có nhiều hơn 0 hay không nêú hơn thì blog đã được tìm kiếm và ngược lại 
             Assert.IsTrue(blogsThang_63.Count > 0, "No blogs found.");
         }
 
 
 
+        // Phương thức dọn dẹp sau mỗi bài kiểm tra (test) để giải phóng tài nguyên và đóng trình duyệt
         [TestCleanup]
         public void TearDownThang_63()
         {
+            // Kiểm tra xem trình điều khiển trình duyệt có tồn tại không trước khi thực hiện dọn dẹp
             if (driverThang_63 != null)
             {
+                // Đóng trình duyệt
                 driverThang_63.Quit();
             }
         }
--- TestDivuiWebsite/UpdateUser.cs
+++ TestDivuiWebsite/UpdateUser.cs
@@ -13,63 +13,87 @@ namespace TestDivuiWebsite
     [TestClass]
     public class UpdateUser
     {
-        private static IWebDriver driverThang_63;
+        // Khai báo một biến driver để sử dụng cho việc điều khiển của trình duyệt web
+        private IWebDriver driverThang_63;
+        //Khai báo một thể hiện của lớp Login đã được viết trước đó để thực hiện việc đăng nhập
         private Login loginThang_63 = new Login();
 
+        // Phương thức thiết lập trước mỗi bài kiểm tra (test) để chuẩn bị môi trường và dữ liệu cần thiết
+        //Phương thức này dùng để truy cập vào các bước đầu tiên chung của các test case 
         [TestInitialize]
         public void SetUpThang_63()
         {
+            //Gọi của phương thức của login để tiến hành đăng nhập 
             loginThang_63.SetUp();
             driverThang_63 = loginThang_63.GetDriver();
             loginThang_63.LoginSuccess();
+            //Lấy element dropdown của biểu tượng tài khoản để hiển thị các chức năng của tài khoản 
             IWebElement userBtnThang_63 = driverThang_63.FindElement(By.CssSelector("li.dropdown:nth-child(3)"));
+            //Tính hành tương tác với element
             userBtnThang_63.Click();
             Thread.Sleep(1000);
+
+            //Lấy element của liên kết "cấu hình tài khoản" và truy cập vào trang cấu hình để tiến hành test chức năng 
             IWebElement userCustomThang_63 = driverThang_63.FindElement(By.XPath("//a[@href='/customer/info']"));
             userCustomThang_63.Click();
             Thread.Sleep(1000);
         }
 
+        //Phương thức này dùng để nhập các gía trị của thông tin người dùng 
         private void inputValueThang_63 (string dataFirstNameThang_63,string dataLastNameThang_63,string dataPhoneThang_63 ,string dataEmailThang_63) 
         {
+            //Lấy ô selectbox của ô danh tính bằng Id 
             IWebElement selectFirstNameThang_63 = driverThang_63.FindElement(By.Id("FirstName"));
             selectFirstNameThang_63.Click();
+            //Lấy các select của ô danh tính bằng đối tượng SelectElement của Selenium
             SelectElement firstNameSelectThang_63 = new SelectElement(selectFirstNameThang_63);
             firstNameSelectThang_63.SelectByText(dataFirstNameThang_63);
             Thread.Sleep(500);
+            //Lấy element của ô tên người dùng và nhập dữ liệu cần kiểm tra
             IWebElement inputLastNameThang_63 = driverThang_63.FindElement(By.Id("LastName"));
             inputLastNameThang_63.Clear();
             inputLastNameThang_63.SendKeys(dataLastNameThang_63);
+            //Lấy element của ô số điện thoại và nhập dữ liệu số điện thoại cần kiểm tra 
             IWebElement inputPhoneThang_63 = driverThang_63.FindElement(By.Id("Phone"));
             inputPhoneThang_63.Clear();
             inputPhoneThang_63.SendKeys(dataPhoneThang_63);
+            //Lấy element của ô số email và nhập dữ liệu số email cần kiểm tra 
             IWebElement inputEmailThang_63 = driverThang_63.FindElement(By.Id("Email"));
             inputEmailThang_63.Clear();
             inputEmailThang_63.SendKeys(dataEmailThang_63);
             Thread.Sleep(1000);
+            //Lấy element của nút submit và click để tiến hành cập nhật thông tin của người dùng
             IWebElement submitBtnThang_63 = driverThang_63.FindElement(By.CssSelector("input[type=submit]"));
             submitBtnThang_63.Click();
         }
 
+        //Phương thức này dùng để cập nhật lại thông tin của người dùng như dữ liệu ban đầu
+        //tiến hành thực hiện các test case khác
         private void ResetUserThang_63()
         {
+            //Các dữ liệu ban đầu
                 string dataFirstNameThang_63 = "Ông";
                 string dataLastNameThang_63 = "Cao Ngọc Sơn";
                 string dataPhoneThang_63 = "";
                 string dataEmailThang_63 = "testtest123@yopmail.com";
+            //Truyền dữ liệu vào hàm cập nhật thông tin đã viết bên trên để tiến hành cập nhật 
                 inputValueThang_63(dataFirstNameThang_63, dataLastNameThang_63, dataPhoneThang_63, dataEmailThang_63);
         }
 
+        //Phương thức này dùng để kiểm tra dữ liệu có được cập nhật với các dữ liệu đúng
         [TestMethod]
         public void updateUserSuccessThang_63()
         {
+            //Các dữ liệu cần kiểm tra 
             string dataFirstNameThang_63 = "Bà";
             string dataLastNameThang_63 = "Đặng Trung Thắng";
             string dataPhoneThang_63 = "0385998378";
             string dataEmailThang_63 = "thangdt61203@gmail.com";
+            //Truyền dữ liệu vào hàm cập nhật thông tin đã viết bên trên để tiến hành cập nhật 
             inputValueThang_63(dataFirstNameThang_63, dataLastNameThang_63, dataPhoneThang_63, dataEmailThang_63);
 
-
+            //Lấy element của các ô đã cập nhật để kiểm tra dữ liệu có được cập nhật thành công hay không 
+            //Nếu dữ liệu sau khi cập nhật và dữ liệu cần cập nhật giống nhau thì testcase thành công và ngược lại thì thất bại
             IWebElement selectFirstNameThang_63 = driverThang_63.FindElement(By.Id("FirstName"));
             SelectElement firstNameSelectThang_63 = new SelectElement(selectFirstNameThang_63);
             Assert.AreEqual(dataFirstNameThang_63, firstNameSelectThang_63.SelectedOption.GetAttribute("innerText"));
@@ -81,40 +105,55 @@ namespace TestDivuiWebsite
             Assert.AreEqual(dataPhoneThang_63, inputPhoneThang_63.GetAttribute("value"));
         }
 
+        //Phương thức này dùng để kiểm tra chức năng có kiểm tra với dữ liệu số điện thoại không đúng định dạng 
         [TestMethod]
         public void updatePhoneInvalidThang_63()
         {
+            //Các dữ liệu cần kiểm tra 
             string dataFirstNameThang_63 = "Bà";
             string dataLastNameThang_63 = "Đặng Trung Thắng";
-            string dataPhoneThang_63 = "123";
+            //Dữ liệu phone truyền vào được viết sai định dạng 
+            string dataPhoneThang_63 = "123dd";
             string dataEmailThang_63 = "thangdt61203@gmail.com";
-
+            //Truyền dữ liệu vào hàm cập nhật thông tin đã viết bên trên để tiến hành cập nhật 
             inputValueThang_63(dataFirstNameThang_63, dataLastNameThang_63, dataPhoneThang_63, dataEmailThang_63);
 
+            //Lấy element thông báo lỗi của web bằng CssSelector 
             IWebElement erorrThang_63 = driverThang_63.FindElement(By.CssSelector("span.field-validation-error>span"));
+            //Kiểm tra thông báo lỗi có xuất hiện hay không nếu có thì test case thành công và ngược lại thì thất bại 
             Assert.IsNotNull(erorrThang_63, "Error message not found, Test Case fail ");
         }
 
+        //Phương thức này dùng để kiểm tra chức năng có kiểm tra với dữ liệu email không đúng định dạng 
         [TestMethod]
         public void updateEmailInvalidThang_63()
         {
+            //Các dữ liệu cần kiểm tra 
             string dataFirstNameThang_63 = "Bà";
             string dataLastNameThang_63 = "Đặng Trung Thắng";
             string dataPhoneThang_63 = "0385998378";
+            //Dữ liệu email truyền vào được viết sai định dạng
             string dataEmailThang_63 = "thangdt61203@gmail";
-
+            //Truyền dữ liệu vào hàm cập nhật thông tin đã viết bên trên để tiến hành cập nhật 
             inputValueThang_63(dataFirstNameThang_63, dataLastNameThang_63, dataPhoneThang_63, dataEmailThang_63);
-
+            
+            //Lấy element thông báo lỗi của web bằng CssSelector 
             IWebElement erorrThang_63 = driverThang_63.FindElement(By.CssSelector("span.field-validation-error>span"));
+            //Kiểm tra thông báo lỗi có xuất hiện hay không nếu có thì test case thành công và ngược lại thì thất bại 
             Assert.IsNotNull(erorrThang_63, "Error message not found, Test Case fail ");
         }
 
+        // Phương thức dọn dẹp sau mỗi bài kiểm tra (test) để giải phóng tài nguyên và đóng trình duyệt
         [TestCleanup]
         public void TearDownThang_63()
         {
+            // Kiểm tra xem trình điều khiển trình duyệt có tồn tại không trước khi thực hiện dọn dẹp
             if (driverThang_63 != null)
             {
+                // Thiết lập lại người dùng trước khi đóng trình duyệt
                 ResetUserThang_63();
+
+                // Đóng trình duyệt
                 driverThang_63.Quit();
             }
         }
